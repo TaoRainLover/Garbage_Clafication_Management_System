c4dc1336ddd73e6361f8edd7a04262aec43e4839
taorainlover <2637050370@qq.com>
2022-04-21 13:46:47 +0800

    初始化仓库

---
+++ src/components/page/NewsForm.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item> <i class="el-icon-lx-add"></i> 新闻阅读材料管理 </el-breadcrumb-item>
+        <el-breadcrumb-item>添加新闻</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="form-box">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="标题">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+
+          <el-form-item label="内容">
+            <el-input type="textarea" rows="5" v-model="form.desc"></el-input>
+          </el-form-item>
+
+          <el-form-item label="来源">
+            <el-input v-model="form.source"></el-input>
+          </el-form-item>
+
+          <el-form-item label="网络链接">
+            <el-input v-model="form.link"></el-input>
+          </el-form-item>
+
+          <el-form-item label="封面路径">
+            <el-input v-model="form.cover_path"></el-input>
+          </el-form-item>
+
+          <el-form-item label="类型">
+            <el-select v-model="form.region" placeholder="请选择">
+              <el-option key="bbk" label="时政类" value="时政类"></el-option>
+              <el-option key="xtc" label="环保类" value="环保类"></el-option>
+              <el-option key="imoo" label="生活类" value="生活类"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="发布日期">
+            <el-col :span="11">
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="form.date1"
+                value-format="yyyy-MM-dd"
+                style="width: 100%"
+              ></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%"></el-time-picker>
+            </el-col>
+          </el-form-item>
+
+          <!-- <el-form-item label="城市级联">
+            <el-cascader :options="options" v-model="form.options"></el-cascader>
+          </el-form-item> -->
+
+          <el-form-item label="是否可见">
+            <el-switch v-model="form.is_visible"></el-switch>
+          </el-form-item>
+
+          <!-- <el-form-item label="多选框">
+            <el-checkbox-group v-model="form.type">
+              <el-checkbox label="步步高" name="type"></el-checkbox>
+              <el-checkbox label="小天才" name="type"></el-checkbox>
+              <el-checkbox label="imoo" name="type"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item> -->
+
+          <!-- <el-form-item label="单选框">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="步步高"></el-radio>
+              <el-radio label="小天才"></el-radio>
+              <el-radio label="imoo"></el-radio>
+            </el-radio-group>
+          </el-form-item> -->
+
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">表单提交</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'baseform',
+  data() {
+    return {
+      options: [
+        {
+          value: 'guangdong',
+          label: '广东省',
+          children: [
+            {
+              value: 'guangzhou',
+              label: '广州市',
+              children: [
+                {
+                  value: 'tianhe',
+                  label: '天河区'
+                },
+                {
+                  value: 'haizhu',
+                  label: '海珠区'
+                }
+              ]
+            },
+            {
+              value: 'dongguan',
+              label: '东莞市',
+              children: [
+                {
+                  value: 'changan',
+                  label: '长安镇'
+                },
+                {
+                  value: 'humen',
+                  label: '虎门镇'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          value: 'hunan',
+          label: '湖南省',
+          children: [
+            {
+              value: 'changsha',
+              label: '长沙市',
+              children: [
+                {
+                  value: 'yuelu',
+                  label: '岳麓区'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      form: {
+        title: '',
+        content: '',
+        date: '',
+        source: '',
+        link: '',
+        cover_path: '',
+        // type: ['步步高'],
+        type: '环保类',
+        is_visible: true
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$message.success('提交成功！');
+    }
+  }
+};
+</script>
---
+++ src/components/page/UserTable.vue
@@ -0,0 +1,222 @@
+<template>
+  <div>
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item> <i class="el-icon-lx-people"></i> 用户管理 </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="container">
+      <div class="handle-box">
+        <el-input v-model="query.name" placeholder="请输入用户名或者openid" class="handle-input mr10"></el-input>
+        <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+      </div>
+      <el-table
+        :data="userData"
+        border
+        class="table"
+        ref="multipleTable"
+        header-cell-class-name="table-header"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" align="center"></el-table-column>
+        <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
+        <el-table-column prop="openid" label="OPENID" align="center"></el-table-column>
+        <el-table-column prop="nickname" label="用户名"></el-table-column>
+
+        <el-table-column label="头像(查看大图)" align="center">
+          <template slot-scope="scope">
+            <el-image class="table-td-thumb" :src="scope.row.avatarUrl" :preview-src-list="[scope.row.avatarUrl]"></el-image>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="积分数量">
+          <template slot-scope="scope">{{ scope.row.credit }}</template>
+        </el-table-column>
+
+        <el-table-column label="状态" align="center">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.status === '正常' ? 'success' : scope.row.status === '失败' ? 'danger' : ''">{{
+              scope.row.status
+            }}</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="regTime" label="注册时间"></el-table-column>
+
+        <el-table-column label="操作" width="180" align="center">
+          <template slot-scope="scope">
+            <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 底部页码 -->
+      <div class="pagination">
+        <el-pagination
+          background
+          layout="total, prev, pager, next"
+          :current-page="query.pageIndex"
+          :page-size="query.pageSize"
+          :total="pageTotal"
+          @current-change="handlePageChange"
+        ></el-pagination>
+      </div>
+    </div>
+
+    <!-- 编辑弹出框 -->
+    <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
+      <el-form ref="form" :model="form" label-width="70px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveEdit">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { fetchData } from '../../api/index';
+export default {
+  name: 'basetable',
+  data() {
+    return {
+      query: {
+        name: '',
+        pageIndex: 1,
+        pageSize: 10
+      },
+      tableData: [],
+      userData: [
+        {
+          id: 1,
+          openid: 'sjfjhfh-safjjf',
+          nickname: 'rainlover',
+          credit: 100,
+          avatarUrl:
+            'https://thirdwx.qlogo.cn/mmopen/vi_32/jHxzkmTaV0xCHepJKiajxTSzLtnlibr3f5Ktc6PR8LqrDOl1FDdzDdflXHF6p3yW7WvsWRcpfPAPWTJqw7A0Z6xQ/132',
+          status: '正常',
+          regTime: '2022-4-20'
+        },
+        {
+          id: 1,
+          openid: 'sjfjhfh-safjjf',
+          nickname: 'rainlover',
+          credit: 100,
+          avatarUrl:
+            'https://thirdwx.qlogo.cn/mmopen/vi_32/mcKlbUAgeHQfVXibqichMm8wpnf7sm3KVoVnDj7k6vJGgquAAFbsy53AjymstSxhZZIBRdpibV6Nycm00RWXVqk3Q/132',
+          status: '正常',
+          regTime: '2022-4-20'
+        }
+      ],
+      multipleSelection: [],
+      delList: [],
+      editVisible: false,
+      pageTotal: 0,
+      form: {},
+      idx: -1,
+      id: -1
+    };
+  },
+  created() {
+    this.getData();
+    console.log(this.tableData);
+  },
+  methods: {
+    // 获取 easy-mock 的模拟数据
+    getData() {
+      fetchData(this.query).then((res) => {
+        console.log(res);
+        this.tableData = res.list;
+        this.pageTotal = res.pageTotal || 50;
+      });
+    },
+    // 触发搜索按钮
+    handleSearch() {
+      this.$set(this.query, 'pageIndex', 1);
+      this.getData();
+    },
+    // 删除操作
+    handleDelete(index, row) {
+      // 二次确认删除
+      this.$confirm('确定要删除吗？', '提示', {
+        type: 'warning'
+      })
+        .then(() => {
+          this.$message.success('删除成功');
+          this.userData.splice(index, 1);
+        })
+        .catch(() => {});
+    },
+    // 多选操作
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    delAllSelection() {
+      const length = this.multipleSelection.length;
+      let str = '';
+      this.delList = this.delList.concat(this.multipleSelection);
+      for (let i = 0; i < length; i++) {
+        str += this.multipleSelection[i].name + ' ';
+      }
+      this.$message.error(`删除了${str}`);
+      this.multipleSelection = [];
+    },
+    // 编辑操作
+    handleEdit(index, row) {
+      this.idx = index;
+      this.form = row;
+      this.editVisible = true;
+    },
+    // 保存编辑
+    saveEdit() {
+      this.editVisible = false;
+      this.$message.success(`修改第 ${this.idx + 1} 行成功`);
+      this.$set(this.tableData, this.idx, this.form);
+    },
+    // 分页导航
+    handlePageChange(val) {
+      this.$set(this.query, 'pageIndex', val);
+      this.getData();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.handle-box {
+  margin-bottom: 20px;
+}
+
+.handle-select {
+  width: 120px;
+}
+
+.handle-input {
+  width: 300px;
+  display: inline-block;
+}
+.table {
+  width: 100%;
+  font-size: 14px;
+}
+.red {
+  color: #ff0000;
+}
+.mr10 {
+  margin-right: 10px;
+}
+.table-td-thumb {
+  display: block;
+  margin: auto;
+  width: 40px;
+  height: 40px;
+}
+</style>
